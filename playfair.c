3291890ef8bfc10053753e12e62eaf9720f6c98d
neverthelessme <122379541+neverthelessme@users.noreply.github.com>
2023-05-24 23:28:06 +0530

    Update playfair.c

--- playfair.c
+++ playfair.c
@@ -1,9 +1,7 @@
-#include <stdio.h>  
+#include <stdio.h>   
 #include <stdlib.h>  
 #include <string.h>  
-
 #define SIZE 30
-
 
 void toLowerCase(char plain[], int ps)
 {
@@ -23,37 +21,34 @@ int removeSpaces(char* plain, int ps)
     plain[count] = '\0';
     return count;
 }
-
-void generateKeyTable(char key[], int ks, char keyT[5][5])
+void Table(char key[], int ks, char m[5][5])
 {
     int i, j, k, flag = 0, *dicty;
-
     dicty = (int*)calloc(26, sizeof(int));
     for (i = 0; i < ks; i++) {
         if (key[i] != 'j')
             dicty[key[i] - 97] = 2;
     }
-
     dicty['j' - 97] = 1;
-
     i = 0;
     j = 0;
-
-    for (k = 0; k < ks; k++) {
-        if (dicty[key[k] - 97] == 2) {
-            dicty[key[k] - 97] -= 1;
-            keyT[i][j] = key[k];
-            j++;
-            if (j == 5) {
-                i++;
-                j = 0;
-            }
-        }
+    for (k = 0; k < ks; k++) 
+    {
+        if (dicty[key[k] - 97] == 2) 
+            {
+            	dicty[key[k] - 97] -= 1;
+            	m[i][j] = key[k];
+            	j++;
+            	if (j == 5) 
+                {
+                	i++;
+               	 j = 0;
+            	}
+        	}
     }
-
     for (k = 0; k < 26; k++) {
         if (dicty[k] == 0) {
-            keyT[i][j] = (char)(k + 97);
+            m[i][j] = (char)(k + 97);
             j++;
             if (j == 5) {
                 i++;
@@ -62,9 +57,7 @@ void generateKeyTable(char key[], int ks, char keyT[5][5])
         }
     }
 }
-
-
-void search(char keyT[5][5], char a, char b, int arr[])
+void search(char m[5][5], char a, char b, int arr[])
 {
     int i, j;
 
@@ -72,25 +65,20 @@ void search(char keyT[5][5], char a, char b, int arr[])
         a = 'i';
     else if (b == 'j')
         b = 'i';
-
     for (i = 0; i < 5; i++) {
-
         for (j = 0; j < 5; j++) {
-
-            if (keyT[i][j] == a) {
+            if (m[i][j] == a) {
                 arr[0] = i;
                 arr[1] = j;
             }
-            else if (keyT[i][j] == b) {
+            else if (m[i][j] == b) {
                 arr[2] = i;
                 arr[3] = j;
             }
         }
     }
 }
-
 int mod5(int a) { return (a % 5); }
-
 int prepare(char str[], int ptrs)
 {
     if (ptrs % 2 != 0) {
@@ -99,70 +87,48 @@ int prepare(char str[], int ptrs)
     }
     return ptrs;
 }
-
-void encrypt(char str[], char keyT[5][5], int ps)
+void encrypt(char str[], char m[5][5], int ps)
 {
     int i, a[4];
-
     for (i = 0; i < ps; i += 2) {
-
-        search(keyT, str[i], str[i + 1], a);
-
+        search(m, str[i], str[i + 1], a);
         if (a[0] == a[2]) {
-            str[i] = keyT[a[0]][mod5(a[1] + 1)];
-            str[i + 1] = keyT[a[0]][mod5(a[3] + 1)];
+            str[i] = m[a[0]][mod5(a[1] + 1)];
+            str[i + 1] = m[a[0]][mod5(a[3] + 1)];
         }
         else if (a[1] == a[3]) {
-            str[i] = keyT[mod5(a[0] + 1)][a[1]];
-            str[i + 1] = keyT[mod5(a[2] + 1)][a[1]];
+            str[i] = m[mod5(a[0] + 1)][a[1]];
+            str[i + 1] = m[mod5(a[2] + 1)][a[1]];
         }
         else {
-            str[i] = keyT[a[0]][a[3]];
-            str[i + 1] = keyT[a[2]][a[1]];
+            str[i] = m[a[0]][a[3]];
+            str[i + 1] = m[a[2]][a[1]];
         }
     }
 }
-
-void encryptByPlayfairCipher(char str[], char key[])
+void encryptBypf(char str[], char key[])
 {
-    char ps, ks, keyT[5][5];
-
-    
+    char ps, ks, m[5][5];
     ks = strlen(key);
     ks = removeSpaces(key, ks);
     toLowerCase(key, ks);
-
-    
     ps = strlen(str);
     toLowerCase(str, ps);
     ps = removeSpaces(str, ps);
-
     ps = prepare(str, ps);
-
-    generateKeyTable(key, ks, keyT);
-
-    encrypt(str, keyT, ps);
+    Table(key, ks, m);
+    encrypt(str, m, ps);
 }
-
 int main()
 {
-    char str[SIZE], key[SIZE];
-    
-    //strcpy(key, "Algorithm");
-    //printf("Key text: %s\n", key);
-
-    //strcpy(str, "Programming");
-    //printf("Plain text: %s\n", str);
-	printf("enter the key \n");
-	scanf("%s",key);
-	
-	printf("enter the sentence \n");
-	//fgets(str,sizeof(str),stdin);
-	scanf("%s",str);
-	
-    encryptByPlayfairCipher(str, key);
-
-    puts(str);
-    return 0;
+	char str[SIZE], key[SIZE];
+	printf("Enter a keyword: \n");
+	fgets(key,sizeof(key),stdin);
+	//scanf("%s",key);
+	printf("Enter a Plaintext to be encrypted: \n");
+	fgets(str,sizeof(str),stdin);
+	//scanf("%s",str);
+	encryptBypf(str,key);
+	printf("Ciphered Text:%s",str);
+	return 0;
 }
-
